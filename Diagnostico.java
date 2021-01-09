fe61c0ffc129f3a4bf207b7a7b8d8e73649ffe1b
JMDelToDi <40222623+JMDelToDi@users.noreply.github.com>
2021-01-09 17:19:08 -0300

    Update Diagnostico.java

--- Diagnostico.java
+++ Diagnostico.java
@@ -1,49 +1,47 @@
+//------------------------------------------------------------//
+//                      MOO - Projeto Final                   //
+//------------------------------------------------------------//
+//                           Grupo 8                          //
+//                                                            //
+// Antônio Sebastian Fernandes Rabelo - NUSP 10797781         //
+// João Marcos Della Torre Divino - NUSP 10377708             //
+// Paulo Inay Cruz - NUSP 10388691                            //
+// Samuel Libardi Godoy - NUSP 9805891                        //
+// Vitor Oliveira Caires - NUSP 10748027                      //
+// Vinícius Molina Garcia - NUSP 8929296                      //
+//------------------------------------------------------------//
+
 package Diagnostico;
 
-
-/**
-* @generated
-*/
 public class Diagnostico {
     
-    /**
-    * @generated
-    */
+    //------------------------------------------------------------//
+    //                          Atributos                         //
+    //------------------------------------------------------------//
     private Double porcentagem_risco;
-    
-    /**
-    * @generated
-    */
     private Date data_diagnostico;
+    //------------------------------------------------------------//
     
     
-    /**
-    * @generated
-    */
+    //------------------------------------------------------------//
+    //                      Setters e getters                     //
+    //------------------------------------------------------------//
     private Double getPorcentagem_risco() {
         return this.porcentagem_risco;
     }
     
-    /**
-    * @generated
-    */
     private Double setPorcentagem_risco(Double porcentagem_risco) {
         this.porcentagem_risco = porcentagem_risco;
     }
     
-    /**
-    * @generated
-    */
     private Date getData_diagnostico() {
         return this.data_diagnostico;
     }
     
-    /**
-    * @generated
-    */
     private Date setData_diagnostico(Date data_diagnostico) {
         this.data_diagnostico = data_diagnostico;
 
     }
+    //------------------------------------------------------------//
     
 }
