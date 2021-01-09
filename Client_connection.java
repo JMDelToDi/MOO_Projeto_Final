ba55a306cf41dc6699c480356779ebdf65308b15
JMDelToDi <40222623+JMDelToDi@users.noreply.github.com>
2021-01-09 17:17:37 -0300

    Update Client_connection.java

--- Client_connection.java
+++ Client_connection.java
@@ -1,38 +1,44 @@
+//------------------------------------------------------------//
+//                      MOO - Projeto Final                   //
+//------------------------------------------------------------//
+//                           Grupo 8                          //
+//                                                            //
+// Antônio Sebastian Fernandes Rabelo - NUSP 10797781         //
+// João Marcos Della Torre Divino - NUSP 10377708             //
+// Paulo Inay Cruz - NUSP 10388691                            //
+// Samuel Libardi Godoy - NUSP 9805891                        //
+// Vitor Oliveira Caires - NUSP 10748027                      //
+// Vinícius Molina Garcia - NUSP 8929296                      //
+//------------------------------------------------------------//
+
 package Client_connection;
 
-
-/**
-* @generated
-*/
 public class Client_connection {
     
-    /**
-    * @generated
-    */
+    //------------------------------------------------------------//
+    //                          Atributos                         //
+    //------------------------------------------------------------//
     private String mensagem;
+    //------------------------------------------------------------//
     
-    /**
-    * @generated
-    */
+    //------------------------------------------------------------//
+    //                      Setters e getters                     //
+    //------------------------------------------------------------//
     private String getMensagem() {
         return this.mensagem;
     }
     
-    /**
-    * @generated
-    */
     private String setMensagem(String mensagem) {
         this.mensagem = mensagem;
     }
+    //------------------------------------------------------------//
     
 
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
+    //------------------------------------------------------------//
+    //                          Operations                        //
+    //------------------------------------------------------------//
     public String enviaMensagem(String msg) {
         //TODO
     }
-    
+    //------------------------------------------------------------//
 }
