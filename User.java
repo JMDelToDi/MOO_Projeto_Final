cfb97e4b801a7b31a028ab3f283f32fdee78fef6
JMDelToDi <40222623+JMDelToDi@users.noreply.github.com>
2021-01-09 17:15:01 -0300

    Update User.java

--- User.java
+++ User.java
@@ -1,208 +1,141 @@
-package User;
+//------------------------------------------------------------//
+//                      MOO - Projeto Final                   //
+//------------------------------------------------------------//
+//                           Grupo 8                          //
+//                                                            //
+// Antônio Sebastian Fernandes Rabelo - NUSP 10797781         //
+// João Marcos Della Torre Divino - NUSP 10377708             //
+// Paulo Inay Cruz - NUSP 10388691                            //
+// Samuel Libardi Godoy - NUSP 9805891                        //
+// Vitor Oliveira Caires - NUSP 10748027                      //
+// Vinícius Molina Garcia - NUSP 8929296                      //
+//------------------------------------------------------------//
 
+package User;
 
 /**
 * @generated
 */
 public class User {
     
-    /**
-    * @generated
-    */
+    //------------------------------------------------------------//
+    //                          Atributos                         //
+    //------------------------------------------------------------//
     private String nome;
-    
-    /**
-    * @generated
-    */
     private String email;
-    
-    /**
-    * @generated
-    */
     private String senha;
-    
-    /**
-    * @generated
-    */
-    private Date data_nascimento;
-    
-    /**
-    * @generated
-    */
-    private Integer idade;
-    
-    /**
-    * @generated
-    */
-    private String sexo;
-    
-    /**
-    * @generated
-    */
-    private String localizacao;
-    
-    
-    /**
-    * @generated
-    */
-    private Set pintas = new HashSet();
-    
-    /**
-    * @generated
-    */
+    private Date data_nascimento;    
+    private Integer idade;    
+    private String sexo;    
+    private String localizacao;    
+    private Set pintas = new HashSet();    
     private Client_connection client_connection;
+    //------------------------------------------------------------//
     
     
-    /**
-    * @generated
-    */
+    //------------------------------------------------------------//
+    //                      Setters e getters                     //
+    //------------------------------------------------------------//
     private String getNome() {
         return this.nome;
     }
     
-    /**
-    * @generated
-    */
     private String setNome(String nome) {
         this.nome = nome;
     }
     
-    /**
-    * @generated
-    */
     private String getEmail() {
         return this.email;
     }
     
-    /**
-    * @generated
-    */
     private String setEmail(String email) {
         this.email = email;
     }
     
-    /**
-    * @generated
-    */
     private String getSenha() {
         return this.senha;
     }
     
-    /**
-    * @generated
-    */
     private String setSenha(String senha) {
         this.senha = senha;
     }
     
-    /**
-    * @generated
-    */
     private Date getData_nascimento() {
         return this.data_nascimento;
     }
     
-    /**
-    * @generated
-    */
     private Date setData_nascimento(Date data_nascimento) {
         this.data_nascimento = data_nascimento;
     }
     
-    /**
-    * @generated
-    */
     private Integer getIdade() {
         return this.idade;
     }
     
-    /**
-    * @generated
-    */
     private Integer setIdade(Integer idade) {
         this.idade = idade;
     }
     
-    /**
-    * @generated
-    */
     private String getSexo() {
         return this.sexo;
     }
     
-    /**
-    * @generated
-    */
     private String setSexo(String sexo) {
         this.sexo = sexo;
     }
     
-    /**
-    * @generated
-    */
     private String getLocalizacao() {
         return this.localizacao;
     }
     
-    /**
-    * @generated
-    */
     private String setLocalizacao(String localizacao) {
         this.localizacao = localizacao;
     }
     
-    public Set getPintas() {
-        return Collection.unmodifiableSet(pintas);
-    }
+    //------------------------------------------------------------//
+    //               Codigos proprios adicionados                 //
+    //------------------------------------------------------------//
+    public Set getPintas() {                                      //
+        return Collection.unmodifiableSet(pintas);                //
+    }                                                             //
+                                                                  //
+    public Pinta adicionarPinta(Pinta pinta) {                    //
+        this.pintas.add(pinta);                                   //
+    }                                                             //
+                                                                  //
+    public void removerPinta(Pinta pinta) {                       //
+        this.pintas.remove(pinta);                                //
+    }                                                             //
+    //------------------------------------------------------------//
     
-    public Pinta adicionarPinta(Pinta pinta) {
-        this.pintas.add(pinta);
-    }
-
-    public void removerPinta(Pinta pinta) {
-        this.pintas.remove(pinta);
-    }
-    
-    /**
-    * @generated
-    */
     public Client_connection getClient_connection() {
         return this.client_connection;
     }
     
-    /**
-    * @generated
-    */
     public Client_connection setClient_connection(Client_connection client_connection) {
         this.client_connection = client_connection;
     }
+    //------------------------------------------------------------//
     
-
-    //                          Operations                                  
     
-    /**
-    * @generated
-    */
+    //------------------------------------------------------------//
+    //                          Operations                        //
+    //------------------------------------------------------------//
     public Set listarPintas() {
         //TODO
     }
-    /**
-    * @generated
-    */
+    
     public Pinta buscarPinta(String nomePinta) {
         //TODO
     }
-    /**
-    * @generated
-    */
+    
     public Pinta criarPinta(String nomePinta) {
         //TODO
     }
-    /**
-    * @generated
-    */
+    
     public String criarMensagemCadastro() {
         //TODO
     }
+    //------------------------------------------------------------//
     
 }
