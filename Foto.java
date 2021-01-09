0cd3b3b2df67a219a407f426b0a496c78a4e34fb
JMDelToDi <40222623+JMDelToDi@users.noreply.github.com>
2021-01-09 17:21:34 -0300

    Update Foto.java

--- Foto.java
+++ Foto.java
@@ -1,90 +1,72 @@
+//------------------------------------------------------------//
+//                      MOO - Projeto Final                   //
+//------------------------------------------------------------//
+//                           Grupo 8                          //
+//                                                            //
+// Antônio Sebastian Fernandes Rabelo - NUSP 10797781         //
+// João Marcos Della Torre Divino - NUSP 10377708             //
+// Paulo Inay Cruz - NUSP 10388691                            //
+// Samuel Libardi Godoy - NUSP 9805891                        //
+// Vitor Oliveira Caires - NUSP 10748027                      //
+// Vinícius Molina Garcia - NUSP 8929296                      //
+//------------------------------------------------------------//
+
 package Foto;
 
-
-/**
-* @generated
-*/
 public class Foto {
     
-    /**
-    * @generated
-    */
+    //------------------------------------------------------------//
+    //                          Atributos                         //
+    //------------------------------------------------------------//
     private File png;
-    
-    
-    /**
-    * @generated
-    */
     private Client_connection client_connection;
-    
-    /**
-    * @generated
-    */
     private Diagnostico diagnostico;
+    //------------------------------------------------------------//
     
     
-    /**
-    * @generated
-    */
+    //------------------------------------------------------------//
+    //                      Setters e getters                     //
+    //------------------------------------------------------------//
     private File getPng() {
         return this.png;
     }
     
-    /**
-    * @generated
-    */
     private File setPng(File png) {
         this.png = png;
     }
     
-    /**
-    * @generated
-    */
     public Diagnostico getDiagnostico() {
         return this.diagnostico;
     }
     
-    /**
-    * @generated
-    */
     public Diagnostico setDiagnostico(Diagnostico diagnostico) {
         this.diagnostico = diagnostico;
     }
     
-    /**
-    * @generated
-    */
     public Client_connection getClient_connection() {
         return this.client_connection;
     }
     
-    /**
-    * @generated
-    */
     public Client_connection setClient_connection(Client_connection client_connection) {
         this.client_connection = client_connection;
     }
-    
+    //------------------------------------------------------------//
 
-    //                          Operations                                  
     
-    /**
-    * @generated
-    */
+    //------------------------------------------------------------//
+    //                          Operations                        //
+    //------------------------------------------------------------//
     public Diagnostico obterProbabilidade() {
         //TODO
     }
-    /**
-    * @generated
-    */
+
     public String criarMensagemProbabilidade() {
         //TODO
     }
-    /**
-    * @generated
-    */
+
     public String criarMensagemValidade() {
         //TODO
     }
+    //------------------------------------------------------------//
     
 }
